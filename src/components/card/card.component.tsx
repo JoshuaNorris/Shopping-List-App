f92d048006d30d098dee5f04d1f1db7d5ee83188
Joshua Norris <joshua.norris52@gmail.com>
2024-10-07 19:16:09 -0500

    finishing cord component.

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import './card.styles.scss';
 
+type CardProps = {};
 
-export const Card = () => {
+export const Card:React.FC<CardProps> = () => {
+
+    const ingredientsClick = () => {
+
+    }
+
+    const recipeClick = () => {
+
+    }
+
+
     return (
         <div className='card'>
             <img className="card-image" src="https://www.w3schools.com/howto/img_avatar.png" alt="John Doe"/>
-            <div className="card-info-container">
-                <h4 className="card-title">John Doe</h4>
-                <p className="card-description">Architect and Engineer</p>
-            </div>
+            <h4 className="card-title">John Doe</h4>
+            <button onClick={ingredientsClick} className="ingredients-btn card-btn">Ingredients</button>
+            <button onClick={recipeClick} className="recipe-btn card-btn">See Complete Recipe</button>
         </div>
     );
 }
